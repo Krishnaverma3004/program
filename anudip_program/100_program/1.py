0e9d8570f77caf7bcce468b3aab265bb7af444aa
krishnaverma <kv391904@gmail.com>
2026-02-24 10:11:03 +0530

    Add 100_program folder

---
+++ anudip_program/100_program/1.py
@@ -0,0 +1,20 @@
+# take input from the user 
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+# Swapping
+a = a + b
+b = a - b
+a = a - b
+
+# printing
+print("After swapping:")
+print("First number:", a)
+print("Second number:", b)
+
+# ouput -------
+# Enter first number: 10
+# Enter second number: 20
+# After swapping:
+# First number: 20
+# Second number: 10
