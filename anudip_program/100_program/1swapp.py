ce1d2aa98ca1d979f11330108d7ce4a78997837c
krishnaverma <kv391904@gmail.com>
2026-02-24 12:03:06 +0530

    Add 100_program folder

--- anudip_program/100_program/1swapp.py
+++ anudip_program/100_program/1swapp.py
@@ -1,3 +1,4 @@
+# Problem Statement: Swap two numbers without using a third variable.
 # take input from the user 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
@@ -7,14 +8,16 @@ a = a + b
 b = a - b
 a = a - b
 
-# printing
+# printing the values 
 print("After swapping:")
 print("First number:", a)
 print("Second number:", b)
 
-# ouput -------
-# Enter first number: 10
-# Enter second number: 20
-# After swapping:
-# First number: 20
-# Second number: 10
+
+
+# output
+ '''Enter first number: 10
+Enter second number: 20
+After swapping:
+First number: 20
+Second number: 10'''
